94d3176578e7f4ea152220f458d727f651106104
corps002 <corps002@morris.umn.edu>
2019-09-03 20:08:38 -0500

    adds comments to the files

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+# Extract the tar archive into the scratch directory
 tar xf $1 -C $2
 
 direct=$(pwd)
 
 cd $2
 
+# Set file as all files with 'DELETE ME!'
 file=$(grep -Rl 'DELETE ME!')
 
+# Loop that removes all files with 'DELETE ME!'
 for file in "${file}"
 do
     rm $file
 done
 
+# Cleaned files are now in working directory under cleaned + whatever the original name was
 tar -czf "$direct/cleaned_$1" $(basename $1 .tgz)
--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# Set the starting directory
 start_directory=$(pwd)
 
 cd $2
 
+# Extract the NthPrime file into a specified directory
 tar xf $start_directory/NthPrime.tgz
 
+# Compiles the C files into the NthPrime Directory
 gcc -o $2/NthPrime/NthPrime $2/NthPrime/main.c $2/NthPrime/nth_prime.c
 
+# Runs NthPrime with a specified number
 NthPrime/NthPrime $1
